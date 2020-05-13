fa9ea7e962b1b9d968cc289b48841bafe4752ae6
ab3lson <ablec@byu.edu>
2020-05-12 21:08:48 -0600

    auto deletes first extracted file

--- fileExtractor.py
+++ fileExtractor.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
 from zipfile import ZipFile
+import os
 
 choices = ['y','Y','n','N']
-filePath = input("What file do you want to extract:")
+noOptions = ['n','N']
+filePath = input("What file do you want to unzip:")
+customTarget = None
+while customTarget not in choices:
+    customTarget = input("Do you want to extract these files somewhere other than your current directory? (Y/N):")
+if customTarget not in noOptions:
+    customTarget = input("What path do you want to extract to?:")
 extractOption = None
 with ZipFile(filePath, 'r') as zipFile:
     fileNames = zipFile.namelist()
@@ -15,6 +22,10 @@ with ZipFile(filePath, 'r') as zipFile:
             if extractOption == ("y" or "Y"):
                 zipFile.extract(fileName,fileName[:-4])
                 with ZipFile(f"{fileName[:-4]}/{fileName}", 'r') as childZip:
-                    childZip.extractall(f"{fileName[:-4]}")
+                    if customTarget not in noOptions:
+                        childZip.extractall(f"{customTarget}/{fileName[:-4]}")
+                    else:
+                        childZip.extractall(f"{fileName[:-4]}")
                     print(f"Extracting, {fileName}")
-
+                print(f"Deleting file: {fileName[:-4]}")
+                os.remove(f"{fileName[:-4]}/{fileName}")
